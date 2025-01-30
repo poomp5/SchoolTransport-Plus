4bdf4b33f82d1480ff383e307655bfd9f8598153
poomp5 <poom27200@gmail.com>
2025-01-30 12:30:20 +0700

    fix responsive

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -2,7 +2,7 @@
 
 import AdminSidebar from "@/components/admin-sidebar";
 import { useState } from "react";
-import { Bus, User, Megaphone } from "lucide-react";
+import { Bus, User, Megaphone, CloudUpload } from "lucide-react";
 import Swal from "sweetalert2";
 
 export default function Settings() {
@@ -22,13 +22,13 @@ export default function Settings() {
             <div className="flex-1 lg:ml-64 mt-8 p-6">
                 <h1 className="text-2xl font-semibold text-gray-600 mb-6">ตั้งค่าระบบ</h1>
                 <div className="border-b flex">
-                    <button className={`flex items-center gap-2 px-4 py-2 text-gray-700 ${activeTab === "bus" ? "border-b-2 border-red-800 text-red-800" : "hover:text-red-800"}`} onClick={() => setActiveTab("bus")}>
+                    <button className={`flex items-center gap-2 px-4 py-2 text-gray-700 whitespace-nowrap ${activeTab === "bus" ? "border-b-2 border-red-800 text-red-800" : "hover:text-red-800"}`} onClick={() => setActiveTab("bus")}>
                         <Bus className="w-5 h-5" /> รถรับส่ง
                     </button>
-                    <button className={`flex items-center gap-2 px-4 py-2 text-gray-700 ${activeTab === "driver" ? "border-b-2 border-red-800 text-red-800" : "hover:text-red-800"}`} onClick={() => setActiveTab("driver")}>
+                    <button className={`flex items-center gap-2 px-4 py-2 text-gray-700 whitespace-nowrap ${activeTab === "driver" ? "border-b-2 border-red-800 text-red-800" : "hover:text-red-800"}`} onClick={() => setActiveTab("driver")}>
                         <User className="w-5 h-5" /> ผู้ขับขี่
                     </button>
-                    <button className={`flex items-center gap-2 px-4 py-2 text-gray-700 ${activeTab === "announcement" ? "border-b-2 border-red-800 text-red-800" : "hover:text-red-800"}`} onClick={() => setActiveTab("announcement")}>
+                    <button className={`flex items-center gap-2 px-4 py-2 text-gray-700 whitespace-nowrap ${activeTab === "announcement" ? "border-b-2 border-red-800 text-red-800" : "hover:text-red-800"}`} onClick={() => setActiveTab("announcement")}>
                         <Megaphone className="w-5 h-5" /> ประชาสัมพันธ์
                     </button>
                 </div>
@@ -67,9 +67,7 @@ export default function Settings() {
                             <div className="flex items-center justify-center w-full">
                                 <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
                                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                        <svg className="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                            <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                        </svg>
+                                        <CloudUpload className="text-gray-500 w-8 h-8"/>
                                         <p className="mb-2 text-sm text-gray-500 "><span className="font-semibold">คลิกเพื่ออัปโหลด</span> หรือลากไฟล์มาวาง</p>
                                         <p className="text-xs text-gray-500 ">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                                     </div>
